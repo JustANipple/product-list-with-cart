1016c377c690779289bb4fa8f7ec73d2d035971e
JustANipple <prandisamuel99@gmail.com>
2025-02-18 20:26:24 +0100

    moved images to public folder. Made recipe skeleton component

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,36 @@ const datas = jsonData;
 <template>
     <main>
         <h1>Desserts</h1>
-        <recipe
-            v-for="data in datas"
-            :image="data.image.mobile"
-            :category="data.category"
-            :name="data.name"
-            :price="data.price"
-        />
+        <div>
+            <recipe
+                v-for="data in datas"
+                :image="data.image.mobile"
+                :category="data.category"
+                :name="data.name"
+                :price="data.price"
+            />
+        </div>
         <cart />
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+    background-color: var(--rose-50);
+    padding: 24px;
+    display: grid;
+    row-gap: 32px;
+}
+
+h1 {
+    color: var(--rose-900);
+    font-weight: var(--fw-bold);
+    line-height: 120%;
+    font-size: 40px;
+}
+
+div {
+    display: grid;
+    row-gap: 24px;
+}
+</style>
--- src/components/recipe.vue
+++ src/components/recipe.vue
@@ -1,13 +1,88 @@
 <script setup>
-defineProps(["image", "category", "name", "price"]);
+import { computed } from "vue";
+
+const props = defineProps(["image", "category", "name", "price"]);
+
+const priceWithDecimal = computed(() => {
+    return props.price.toFixed(2);
+});
 </script>
 
 <template>
-    <div>Recipe</div>
-    <p>{{ image }}</p>
-    <p>{{ category }}</p>
-    <p>{{ name }}</p>
-    <p>{{ price }}</p>
+    <div class="recipe">
+        <div class="recipeTop">
+            <img :src="props.image" alt="recipe image" class="recipeImage" />
+            <button type="button" class="addButton">
+                <img src="/images/icon-add-to-cart.svg" alt="cart icon" />
+                Add to cart
+            </button>
+        </div>
+        <div class="recipeBottom">
+            <span class="recipeCategory">{{ props.category }}</span>
+            <p class="recipeName">{{ props.name }}</p>
+            <p class="recipeTitle">${{ priceWithDecimal }}</p>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.recipe {
+    display: grid;
+    row-gap: 16px;
+}
+
+.recipeTop {
+    height: 234px;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+    position: relative;
+}
+
+.recipeImage {
+    height: 212px;
+    margin-block-end: auto;
+    border-radius: 8px;
+}
+
+.addButton {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin-inline: auto;
+    max-width: fit-content;
+    display: flex;
+    gap: 8px;
+    border-radius: 2rem;
+    background-color: white;
+    border: 1px solid var(--rose-400);
+    padding-block: 12px;
+    padding-inline: 24px;
+    color: var(--rose-900);
+    font-size: 14px;
+    font-weight: var(--fw-semiBold);
+}
+
+.recipeBottom {
+    display: grid;
+    row-gap: 4px;
+}
+
+.recipeCategory {
+    font-size: 14px;
+    color: var(--rose-500);
+}
+
+.recipeName {
+    font-size: var(--fs-body);
+    font-weight: var(--fw-semiBold);
+    color: var(--rose-900);
+}
+
+.recipeTitle {
+    font-size: var(--fs-body);
+    font-weight: var(--fw-semiBold);
+    color: var(--red);
+}
+</style>
