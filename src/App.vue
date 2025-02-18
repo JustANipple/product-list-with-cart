49ba5a88f2111d03f5b024e97813d4410c24e17a
JustANipple <prandisamuel99@gmail.com>
2025-02-18 19:02:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import jsonData from "./data.json";
+import recipe from "./components/recipe.vue";
+import cart from "./components/cart.vue";
+
+const datas = jsonData;
+</script>
+
+<template>
+    <main>
+        <h1>Desserts</h1>
+        <recipe
+            v-for="data in datas"
+            :image="data.image.mobile"
+            :category="data.category"
+            :name="data.name"
+            :price="data.price"
+        />
+        <cart />
+    </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/recipe.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(["image", "category", "name", "price"]);
+</script>
+
+<template>
+    <div>Recipe</div>
+    <p>{{ image }}</p>
+    <p>{{ category }}</p>
+    <p>{{ name }}</p>
+    <p>{{ price }}</p>
+</template>
+
+<style scoped></style>
